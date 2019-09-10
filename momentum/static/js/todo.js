24f10fcc79f942eac46f98315851f32f2ecc8240
hwb0218 <52212226+hwb0218@users.noreply.github.com>
2019-09-10 19:17:38 +0900

    momentum

--- momentum/static/js/todo.js
+++ momentum/static/js/todo.js
@@ -6,9 +6,15 @@ const TODOS_LS = "toDos";
 
 const toDos = [];
 
-function deleteToDO(event.target){
+function deleteToDo(event){
+	const btn = event.target;
+	const li = btn.parentNode;
+	toDoList.removeChild(li);
+	const cleanToDos = toDos.filter(function(toDo){
+		return toDo.id !== parseInt(li.id);
+	});
+	console.log(cleanToDos);
 
-}
 
 function saveToDos(){
 	localStorage.setItem(TODOS_LS, JSON.stringify(toDos)) // localStorage.setItem(key,value)
@@ -20,7 +26,7 @@ function paintToDo(value) {
 	const span = document.createElement("span");
 	const newId = toDos.length + 1;
 	delBtn.innerText = "❌";
-	delBtn.addEventListener("click", deleteToDO);
+	delBtn.addEventListener("click", deleteToDo);
 	span.innerText = value;
 	li.appendChild(span);
 	li.appendChild(delBtn);
@@ -48,8 +54,8 @@ function loadToDos(){
 	const loadedToDos = localStorage.getItem(TODOS_LS);
 	if(loadedToDos !== null) {
 		const parsedToDos = JSON.parse(loadedToDos);
-		parsedToDos.forEach(function(toDo){
-			paintToDo(toDo.text); // 어떻게
+		parsedToDos.forEach(function(toDo){ // 이함수의 동작은 어떤식?
+			paintToDo(toDo.text);
 		});
 	}
 }
